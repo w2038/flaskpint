6a3f86827a0ec7294de574363097878149cb9b04
w2038 <w4gn3r2038@gmail.com>
2023-09-30 12:54:18 -0300

    reestruturando o codigo

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template, url_for
-
-app = Flask(__name__)
-
-@app.route("/")
-def homepage():
-    return render_template("homepage.html")
-
-@app.route("/perfil")
-def perfil():
-    return render_template("perfil.html")
+from pint import app
 
 if __name__ == "__main__":
     app.run(debug=True)
